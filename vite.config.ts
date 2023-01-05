501c470177196167b2fa8e2fe801b3493152f164
Rafael Franco <rafaelfr@outlook.com>
2023-01-04 21:30:29 -0300

    chore: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/kanashiro',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/kanashiro' : '/',
   plugins: [react()],
-})
+}))
