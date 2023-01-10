47b7245ffa4ee6cebb214eb4fd06df9962c8afef
Rafael Franco <rafaelfr@outlook.com>
2023-01-10 10:02:14 -0300

    chore: test new config

--- functions/authenticate.ts
+++ functions/authenticate.ts
@@ -1,4 +1,19 @@
-export const onRequest = async (context) => {
-  const value = await context.env.AK_PASS.get('senha')
-  return new Response(value)
+import type { KVNamespace, PagesFunction } from '@cloudflare/workers-types'
+interface Env {
+  KV: KVNamespace
+}
+
+export const onPostRequest: PagesFunction<Env> = async ({ env, request }) => {
+  const formData = await request.formData()
+  const key = formData.get('key')
+  const location = formData.get('location')
+
+  if (typeof key === 'string') {
+    const value = await env.KV.get(key)
+    console.log(key, value, location)
+    if (key === value && location)
+      return new Response(null, { status: 301, headers: { location } })
+  }
+
+  return new Response(null, { status: 400 })
 }
