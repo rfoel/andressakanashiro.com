8dd4e839c58d9768eaa5df2c388f31d3420c44ff
Rafael Franco <rafaelfr@outlook.com>
2023-01-10 09:49:36 -0300

    chore: test kv

--- functions/authenticate.ts
+++ functions/authenticate.ts
@@ -1,4 +1,4 @@
 export const onRequest = async (context) => {
-  // const value = await context.env.AK_PASS.get('senha')
-  return new Response('hello')
+  const value = await context.env.AK_PASS.get('senha')
+  return new Response(value)
 }
