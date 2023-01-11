406437f56a99ac7ccc4bf5de5022fcc3f3baa889
Rafael Franco <rafaelfr@outlook.com>
2023-01-11 09:03:58 -0300

    chore: work

--- src/routes/Pass/index.tsx
+++ src/routes/Pass/index.tsx
@@ -1,23 +1,19 @@
-import { useEffect } from 'react'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 export const path = '/pass'
 
 const Element = () => {
-  const location = useLocation()
-  const searchParams = useSearchParams()
-  console.log({ location, searchParams })
-  useEffect(() => {
-    const password = window.prompt('What is the password?')
-    const formData = new FormData()
-    formData.append('password', password || '')
-    formData.append('location', '/')
-    fetch('/authenticate', {
-      credentials: 'include',
-      method: 'post',
-      body: formData,
-    })
-  }, [])
+  const [searchParams] = useSearchParams()
+
+  const password = window.prompt('What is the password?')
+  const formData = new FormData()
+  formData.append('password', password || '')
+  formData.append('location', searchParams.get('location') || '/')
+  fetch('/authenticate', {
+    credentials: 'include',
+    method: 'post',
+    body: formData,
+  })
 
   return null
 }
