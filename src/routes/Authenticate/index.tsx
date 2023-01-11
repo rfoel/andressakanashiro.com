6928fff85703aa76c32f5643e3932e302aee01fc
Rafael Franco <rafaelfr@outlook.com>
2023-01-11 08:47:18 -0300

    chore: test

--- src/routes/Authenticate/index.tsx
+++ src/routes/Authenticate/index.tsx
@@ -1,18 +1,26 @@
-import { LoaderFunction } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
 export const path = '/authenticate'
 
-export const loader: LoaderFunction = ({ request }) => {
-  const url = new URL(request.url)
-  const password = window.prompt('What is the password?')
-  const formData = new FormData()
-  formData.append('password', password || '')
-  formData.append('location', url.searchParams.get('location') || '/')
-  return fetch('/authenticate', {
-    credentials: 'include',
-    method: 'post',
-    body: formData,
-  })
-}
+const Element = () => {
+  const searchParams = useSearchParams()
+  const location = useLocation()
+  console.log({ searchParams })
+  console.log({ location })
 
-export const element = null
+  useEffect(() => {
+    const password = window.prompt('What is the password?')
+    const formData = new FormData()
+    formData.append('password', password || '')
+    formData.append('location', '/')
+    fetch('/authenticate', {
+      credentials: 'include',
+      method: 'post',
+      body: formData,
+    })
+  }, [])
+
+  return null
+}
+export const element = <Element />
